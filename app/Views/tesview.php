f6c9f33f5ff77ef905afbfb14c94229246853a8f
nurlailaxirpl2 <nurlailahusna3@gmail.com>
2022-05-13 10:39:49 +0700

    first commit

---
+++ app/Views/tesview.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document Title</title>
+</head>
+<body>
+    <table>
+        <tr>
+            <th>No</th>
+            <th>Nama</th>
+            <th>Kelas</th>
+         <tr>
+             <th>20</th>
+             <th>Laila</th>
+             <th>XI RPL 2</th>
+         </tr>   
+        </tr>
+    </table>
+</body>
+</html>
